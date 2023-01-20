21e086395219161c6786619a4928e463574591c7
clifford-andrew-stevenson <clifford.andrew.stevenson@gmail.com>
2023-01-19 17:56:15 -0800

    Amazon App

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDFvzV0DBepOsSSdY8qF5CrtsSmUothFX4",
+    authDomain: "clone-5f394.firebaseapp.com",
+    projectId: "clone-5f394",
+    storageBucket: "clone-5f394.appspot.com",
+    messagingSenderId: "270048245385",
+    appId: "1:270048245385:web:1d982a0606992423f37858"
+};
+
